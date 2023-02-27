fcd3c9d15721e657adb40d30eba1211c37d9b7c7
Anderson735 <andersonmendezmarin@gmail.com>
2023-02-27 11:22:57 -0500

    HOME: Added home view

---
+++ src/app/feature/auth/auth.routes.ts
@@ -0,0 +1,8 @@
+import { Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+
+export const AUTH_ROUTES: Routes = [
+    {
+        path: '', component: AuthComponent
+    }
+]
---
+++ src/app/feature/home/components/modalDelete/modalDelete.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modalDelete',
+  templateUrl: './modalDelete.component.html',
+  styleUrls: ['./modalDelete.component.scss']
+})
+export class ModalDeleteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
